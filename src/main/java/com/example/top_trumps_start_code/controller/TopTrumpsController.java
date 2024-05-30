d74d3d1acd27f6f1fe803708598fd697608d80e4
KhanZuh <khanzuhair96@live.co.uk>
2024-05-30 16:06:39 +0100

    MVP part 1

--- src/main/java/com/example/top_trumps_start_code/controller/TopTrumpsController.java
+++ src/main/java/com/example/top_trumps_start_code/controller/TopTrumpsController.java
@@ -2,6 +2,8 @@ package com.example.top_trumps_start_code.controller;
 
 
 import com.example.top_trumps_start_code.models.Card;
+import com.example.top_trumps_start_code.service.TopTrumpsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,13 +11,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping ("play")
 public class TopTrumpsController {
 
+    @Autowired
+    TopTrumpsService topTrumpsService;
+
+//    @PostMapping
+//    public ResponseEntity<ArrayList<Card>> playGame() {
+//        ArrayList <Card> card = topTrumpsService.playGame();
+//        return new ResponseEntity<ArrayList<Card>>(card, HttpStatus.CREATED);
+//    }
+
     @PostMapping
-    public ResponseEntity<Card> playGame() {
-        Card card = topTrumpService.playGame();
-        return new ResponseEntity<>(card, HttpStatus.CREATED);
+    public ResponseEntity<String> playRound(@RequestBody ArrayList<Card> cards) {
+        if (cards.size() <= 2) {
+            return new ResponseEntity<>("Game started", HttpStatus.OK);
+        }
+
+        return null;
     }
+
+
 }
--- src/main/java/com/example/top_trumps_start_code/service/TopTrumpsService.java
+++ src/main/java/com/example/top_trumps_start_code/service/TopTrumpsService.java
@@ -1,4 +1,31 @@
 package com.example.top_trumps_start_code.service;
 
+
+import com.example.top_trumps_start_code.models.Card;
+import com.example.top_trumps_start_code.models.Rank;
+import com.example.top_trumps_start_code.models.Suit;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class TopTrumpsService {
+
+    private Rank rank;
+    private Suit suit;
+    private ArrayList<Card> cards;
+
+    public TopTrumpsService() {
+
+    }
+
+    public String checkWinner
+
+
+
+
+
+
+
+
 }
